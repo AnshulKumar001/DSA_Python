a95bc0f19591a6bbf742dc52657f8ffea7d08085
anshulkumar-web <anshulkumar9368289674@gmail.com>
2025-06-28 21:36:30 +0530

    My first commit for palindrome problem solve

---
+++ polindrom.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+result = 0
+n=num
+while num > 0:
+    digit = num % 10
+    result = result * 10 + digit
+    num = num // 10
+
+if n == result:
+    print("Number is  palindrome.")
+else:
+    print("Number is not palindrome.")
+         
